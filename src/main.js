19d8539cd1cb090998fa48c17eed19ec447cea84
Nishikawa aika <n223t120@st.kyoto-seika.ac.jp>
2026-07-21 00:06:44 +0900

    1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 import mixitup from 'mixitup';
+
+
+const containerEl = document.querySelector('.menu-container');
+
+const mixer = mixitup(containerEl, {
+  selectors: {
+    target: '.mix'
+  },
+  animation: {
+    duration: 300,
+    effects: 'fade translateY(10px)'
+  }
+});
