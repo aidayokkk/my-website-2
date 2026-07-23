dfc1d60f88aa06d10f13784399a34e5c910e34f0
Nishikawa aika <n223t120@st.kyoto-seika.ac.jp>
2026-07-23 15:26:33 +0900

    コミ

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import mixitup from 'mixitup';
 
-// HTML内のメニューコンテナを取得
+
 const containerEl = document.querySelector('.mix-container');
 
-// MixItUpを初期化（初期状態で .food だけを表示するように設定）
+
 const mixer = mixitup(containerEl, {
   selectors: {
     target: '.mix'
